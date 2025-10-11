004b1dcdd896e33a679ce5813e3d6d6a6e4bb410
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-10-11 10:01:43 +0600

    update with offline

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,65 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 import { fileURLToPath } from 'url';
+import { VitePWA } from 'vite-plugin-pwa'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto', 
+      
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg,woff,woff2,ttf,eot}'], 
+        
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => url.origin === location.origin,
+            handler: 'CacheFirst', 
+            options: {
+              cacheName: 'local-app-assets',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 365, 
+              },
+            },
+          },
+        ]
+      },
+      
+      manifest: {
+        name: 'Mouza Map Calculator', 
+        short_name: 'MapCalc', 
+        description: 'An offline tool to measure land from mouza maps.', 
+        theme_color: '#ffffff', 
+        background_color: '#ffffff', 
+        display: 'standalone', 
+        icons: [
+          {
+            src: 'pwa-192x192.png', 
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png', 
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
-    host: '192.168.0.102',
+    host: '192.168.0.100',
     port: 3000,
   },
   resolve: {
